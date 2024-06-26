fc4176d373de29b56730cde032bcd4acfd384ecf
Aayush0330 <147029924+Aayush0330@users.noreply.github.com>
2024-06-26 08:46:09 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =144224
+let accountEmail="aayush@gmail.com"
+var accountPasward="12345"
+accountCity="Goa"
+let accountState;
+
+// accountID =2
+
+accountEmail="ac@gamil.com"
+accountPasward="43111111"
+accountCity="Jaipur"
+
+/*
+   perfer not to use vsr
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPasward,accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node.js  not browser
+
+console.log(3+4)
+// code readablity should be good
+console.log("Aayush")
+
+let name ="aayush"
+let age =20
+let isLoggedIn =false
+
+// number =>2 to power 53
+// bigint
+// string=>""
+// boolean=>true , false
+// null=>standalone value
+// undefined=>value is not assined
+// symbol=>uniquie
+
+
+// object
+
+console.log(typeof "aay")
+console.log(typeof null)
+console.log(typeof undefined)
